5f0c91817a6794b9721de9c1836efaab0def9d32
tangshoubin <tangshoubin@itiger.com>
2025-07-18 16:16:49 +0800

    update

--- internal/controller/agent/agent_v1_job.go
+++ internal/controller/agent/agent_v1_job.go
@@ -8,19 +8,40 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
+// semaphore
+var sem = make(chan struct{}, 1)
+
 func (c *ControllerV1) JobRun(ctx context.Context, req *JobRunReq) (response *ghttp.Response, err error) {
 	r := g.RequestFromCtx(ctx)
 
 	job_id := r.Get("id").String()
 	g.Log().Debug(ctx, "job_id: ", job_id)
 
-	pipeline_body, err := service.Task.Run(job_id)
-	if err != nil {
-		return nil, err
-	}
-	g.Log().Debug(ctx, "pipeline_body: ", pipeline_body)
+	// pipeline_body, err := service.Task.Run(job_id)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// g.Log().Debug(ctx, "pipeline_body: ", pipeline_body)
 
-	r.Response.WriteExit(pipeline_body)
+	select {
+	case sem <- struct{}{}:
+		// 获取执行权限
+		defer func() { <-sem }()
+		// 执行命令
+		task_run_result, err := service.Task.Run(job_id)
+		if err != nil {
+			r.Response.WriteStatusExit(500, err.Error())
+			return nil, err
+		}
+		g.Log().Debug(ctx, "task_run_result: ", task_run_result)
+
+		r.Response.Write(task_run_result)
+
+	case <-ctx.Done():
+		r.Response.WriteStatusExit(429, "too many concurrent commands")
+	}
+
+	// r.Response.WriteExit(pipeline_body)
 	return nil, nil
 }
 
